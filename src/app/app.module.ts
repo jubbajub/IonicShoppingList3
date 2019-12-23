9e18c4da3498d349066c8f8cdc83ff1205d87af0
jubbajub <sefer.derya@gmx.de>
2019-12-23 08:44:13 +0100

    FIREBASE Credentials hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Angular Firebase Credentials hinzufügen
+// Import hinzufügen
+// unten bei NgModule zusätzlich deklarieren
+
+// in Firebase Konsole:
+// 1.Authentication --> Anmeldemethode --> Anonym AKTIVIEREN
+// 2.Database erstellen
+  // umstellen auf Realtime Database
+  // Regeln --> beide ändern auf true
+
+import { AngularFireModule } from 'angularfire2';
+import {  FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,13 @@
+// TERMINAL
+// > npm install firebase angularfire2 --save
+
+export const FIREBASE_CONFIG ={
+    apiKey: "AIzaSyDl3EZfoqit_EcfdfvbOY65EbnMwFdSNso",
+    authDomain: "ionicshoppinglist3.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist3.firebaseio.com",
+    projectId: "ionicshoppinglist3",
+    storageBucket: "ionicshoppinglist3.appspot.com",
+    messagingSenderId: "999437518305",
+    appId: "1:999437518305:web:5eb624ba5a1aabeb467ea0",
+    measurementId: "G-EPGEMSZ5FY"
+}
