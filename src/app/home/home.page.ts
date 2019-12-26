530edad15c438d2aff49039abf269d48ef0175c2
jubbajub <sefer.derya@gmx.de>
2019-12-26 20:23:25 +0100

    NAvPArams gibt es nicht mehr. Wie Paramter übergeb

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,4 +11,13 @@ export class HomePage {
     console.log('SEFER:Home Page geladen!');
   }
 
+  // sobald die Home Page aufgerufen wird, wird diese Methode ausgeführt
+  ionViewWillEnter(){
+    console.log('SEFER:ionViewWillEnter!');
+/*     Der folgende Aufruf von Homepage (von einer anderne Seite) schickt die PAramter:
+    this.router.navigate(['home'], {queryParams: {key: ref.key, item_name: item.name}}); */
+
+    console.log(NavParams.item.name);
+  }
+
 }
