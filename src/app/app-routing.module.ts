51499b96438264a970465feb5c31a4e3496f8bb7
jubbajub <sefer.derya@gmx.de>
2020-01-06 10:55:48 +0100

    bug fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   // Redirect
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  
+
   // Lazy Loaded Route (Page)
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
